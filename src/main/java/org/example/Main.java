d9dc843a6a89d1300febb46f00ac5135cc9ce0b8
atha <maranatha.purba@gmail.com>
2024-02-28 17:32:21 +0700

    Shape Polymorphism exercise

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,18 +3,75 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static <string> void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
         System.out.println("New Line");
 
-        boolean flag;
+ /*       boolean flag;
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+*/        //Data types and variable
+        int a = 10;
+        int b = 2;
+        float c = 3;
+        String name = "Atha";
+        char q = 'Y';
+        boolean flag2 = false; //true
+
+        //Arithm
+        int result = a / b;
+        int result2 = (int) (a / c); //1 int dibagi 1 float --> float lbh tinggi
+        float result3 = a / c;
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+
+        //if, else, else-if
+        int timeStart = 8;
+        int timeFinish = 17;
+        int waktuMasuk = 9;
+        int waktuPulang = 18;
+
+        //Karyawan yang masuk tepat dan pulang tepat waktu atau lebih lama, diselamati
+        if (waktuMasuk <= timeStart && waktuPulang >= timeFinish){
+            System.out.println("Selamat, anda masuk tepat waktu");
+        System.out.print("Anda sampai " + (timeStart - waktuMasuk) + " jam lebih cepat");
+            //Karyawan yang masuk tepat waktu, pulang duluan, dicengin
+        }else if(waktuMasuk <= timeStart && waktuPulang <= timeFinish){
+            System.out.println("yeu pulang duluan");
+            //Karyawan yang telat masuk, tapi pulang lebih lama atau tepat waktu, dimaafkan
+        }else if(waktuMasuk >= timeStart && waktuPulang >= timeFinish){
+            System.out.println("dimaafin telatnya");
+            //Karyawan yang telat masuk, pulang duluan disoraki
+        }else
+        {
+            System.out.println("Yeu!!! Telat...");
+            System.out.print("Anda sampai " + (waktuMasuk-timeStart) + " jam lebih lama");
+        }
+
+        //Java switch
+        int day = 3;
+        char question = 'Y';
+        switch (day){
+            case 1:
+                System.out.print("Saya WFO");
+                break;
+            case 2:
+                System.out.println("Saya cuti");
+                break;
+            case 3:
+                System.out.println("Saya WFH");
+                break;
+            default:
+                System.out.println("Saya ga kerja alias nganggur");
+        }
+
+
     }
 }
---
+++ src/main/java/org/example/ShapeDrawerPolyMorph.java
@@ -0,0 +1,128 @@
+package org.example;
+import java.awt.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+interface Shape { //One of implementation on Polymorphism
+    void draw (int height, int height2, int side);
+        }
+
+class drawTriangleSS implements Shape {
+    @Override
+    public void draw (int height, int height2, int side){
+        for(int i = 1; i <= height; i++){
+            //inner loop
+            for(int j = 1 ; j<=i; j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class drawTriangleSK implements Shape {
+    @Override
+    public void draw (int height, int height2, int side){
+        for (int i = 1; i <= height2; i++) {
+            // Print spaces to center the triangle
+            for (int j = height2 - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            // Print stars for the triangle
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            // Move to the next line after each row
+            System.out.println();
+        }
+    }
+}
+public class ShapeDrawerPolyMorph {
+    Scanner scanner = new Scanner(System.in); //untuk masukin user input
+    public void displayMenu(){
+        System.out.println ("Selamat datang di ASCII draw!");
+        System.out.println ("Pilih bentuk yang mau digambar");
+        System.out.println ("1. Segitiga siku-siku");
+        System.out.println ("2. Segitiga sama kaki");
+        System.out.println ("3. Exit\n");
+    }
+
+    public void drawTriangleSS(int height){
+        //Nested loop
+        //Outer loop
+        for(int i = 1; i <= height; i++){
+            //inner loop
+            for(int j = 1 ; j<=i; j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void drawTriangleSK(int height2){
+        //Nested loop
+        //Outer loop
+        for (int i = 1; i <= height2; i++) {
+            // Print spaces to center the triangle
+            for (int j = height2 - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            // Print stars for the triangle
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            // Move to the next line after each row
+            System.out.println();
+        }
+    }
+    public void DrawShape(int choice){
+        try {
+            switch (choice){
+                case 1:
+                    System.out.println("Masukkan Panjang segitiga siku-siku: ");
+                    int height = scanner.nextInt();
+                    drawTriangleSS(height); //Fungsi untuk menggambar segitiganya
+                    new drawTriangleSS().draw(height, 1,1);
+                    break;
+                case 2:
+                    System.out.println("Masukkan Panjang segitiga sama kaki: ");
+                    int height2 = scanner.nextInt();
+                    drawTriangleSK(height2); //Fungsi untuk menggambar segitiganya
+                    new drawTriangleSK().draw(height2,1,1);
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+        }
+        catch (InputMismatchException ime){
+            System.out.println("Please enter a valid integer.");
+            scanner.next(); //to consume the invalid token
+        }
+    }
+    public void run(){
+        //Loop utk terus menampilkan menu
+        while(true){
+            displayMenu();
+            System.out.println("Masukkan pilihan anda: ");
+            try {
+                int choice = scanner.nextInt(); //Input yg jenisnya integer
+                DrawShape(choice);
+                System.out.println();
+            } catch (InputMismatchException ime){
+                System.out.println("Please enter a valid integer.");
+                scanner.next();
+            }
+        }
+
+    }
+    public static void main(String[] args){
+        //bikin program utk gambar ASCII shape
+        // Runner
+        ShapeDrawerPolyMorph drawer = new ShapeDrawerPolyMorph();
+        drawer.run();
+    }
+
+}
